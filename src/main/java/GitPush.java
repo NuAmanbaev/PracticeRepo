05e38a65d6f5612263f6b4c129c9a1d080541e8c
Nurgazy Amanbaev <nurgazyamanbaev@Nurgazys-Air.hsd1.il.comcast.net>
2024-07-16 21:14:11 -0500

    NurgazyNewBranch

--- src/main/java/GitPush.java
+++ src/main/java/GitPush.java
@@ -1,6 +1,6 @@
 public class GitPush {
     public static void main(String[] args) {
         System.out.println("Hello WOrld!");
-        System.out.println("Nurgazy");
+        System.out.println("Nurgazy");-
     }
 }
